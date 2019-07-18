8db756667ba1e244acf1d57daefd0657c5cc288e
Porrino7 <47321453+Porrino7@users.noreply.github.com>
2019-07-17 22:11:12 -0300

    Add files via upload

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -10,5 +10,105 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  */
 function CalcularPrecio () 
 {
- 	
-}
+    var cantidad;
+    var marca;
+    var descuento;
+    var preciocondescuento;
+    var mensaje;
+
+
+    cantidad=document.getElementById("Cantidad").value;
+    cantidad=parseInt(cantidad);
+    marca=document.getElementById("Marca").value;
+
+    if(cantidad >= 6 ){
+
+        descuento= 35 * 50 / 100 ;
+        preciocondescuento = (cantidad * 35 ) - descuento * cantidad;
+        
+        document.getElementById("precioDescuento").value=preciocondescuento
+       
+
+    
+
+    }else
+    if( cantidad == 5){
+         
+        switch (marca) {
+            case "ArgentinaLuz":
+                        descuento= 35 * 40 / 100 ;
+                        preciocondescuento = (cantidad * 35 ) - descuento * cantidad;
+                        
+                       
+                break;
+            
+            default:
+
+                        descuento= 35 * 30 / 100 ;
+                        preciocondescuento = (cantidad * 35 ) - descuento * cantidad;
+                        
+                        
+                break;}
+                
+    } else if (cantidad == 4){
+                
+        switch (marca) {
+            case "ArgentinaLuz":
+            case "FelipeLamparas":
+                descuento= 35 * 25 / 100 ;
+                preciocondescuento = (cantidad * 35 ) - descuento * cantidad;
+                        
+                        
+                break;
+            
+            default:
+                descuento= 35 * 20 / 100 ;
+                preciocondescuento = (cantidad * 35 ) - descuento * cantidad;
+                        
+                        
+                break;}
+            }
+            
+        else if (cantidad == 3 ){
+            switch (marca) {
+                case "ArgentinaLuz":
+                    descuento= 35 * 15 / 100 ;
+                    preciocondescuento = (cantidad * 35 ) - descuento * cantidad;
+                            
+                          
+                    break;
+                case "FelipeLamparas":
+
+                    descuento= 35 * 10 / 100 ;
+                    preciocondescuento = (cantidad * 35 ) - descuento * cantidad;
+                        
+                            o
+                default:
+
+                    descuento= 35 * 5 / 100 ;
+                    preciocondescuento = (cantidad * 35 ) - descuento * cantidad;
+                            
+                           
+                    break;}
+            }
+
+            document.getElementById("precioDescuento").value=preciocondescuento
+
+
+
+
+
+
+
+
+
+        }
+
+
+    
+        
+
+
+
+
+        
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,46 @@
 function mostrar()
-{
+{   
+    
+    var planeta;
+    var mensaje;
+
+    planeta=prompt("ingrese planeta");
+
+    switch (planeta) {
+        case "Tierra":
+            alert("vivimos aca")
+            
+            break;
+        
+        case "Venus":
+        case "Mercurio":
+            alert("aca hace mas calor");
+
+            break;
+        
+        case "Marte":
+        case "Jupiter": 
+        case"Urano":
+        case "Saturno":
+            
+            alert("aca hace mas frio");
+        
+            break;
+            default:
+                
+                alert("esto no es un planeta");
+                
+                break;
+        
+        
+        
+        
+        }
+    
+       
+    
+
+
+
 
 }
